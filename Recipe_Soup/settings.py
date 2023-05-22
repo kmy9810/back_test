a1562925cefd36575a4374632c13535ce7d57b4f
Msgun7 <fgarr@naver.com>
2023-05-22 16:28:08 +0900

    소셜 로그인 초기셋팅

--- Recipe_Soup/settings.py
+++ Recipe_Soup/settings.py
@@ -71,6 +71,15 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
