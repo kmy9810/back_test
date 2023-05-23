22c5e80e40115632ee994cc3341e797edb66a53c
Soeun_Jang <azulcielo_@naver.com>
2023-05-23 11:18:41 +0900

    request 라이브러리 설치, pay모델, subscribe모델, urls.py 추가, view.py 추가

--- pay/models.py
+++ pay/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    supplied_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    # 다른 필드들을 필요에 따라 추가할 수 있습니다.
+    point = models.DecimalField(max_digits=10, decimal_places=2)
+    
+
+    def __str__(self):
+        return f"결제 금액:{self.amount} Point: {self.point}"
+
+
+class Subscribe(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
+    is_subscribe = models.BooleanField(default=False)
+    start_subscribe_at = models.DateTimeField(auto_now_add=True)
+    restart_subscribe_at = models.DateTimeField(null=True, default=None, blank=True)
+    end_subscribe_at = models.DateTimeField(null=True, default=None, blank=True)
+    price = models.PositiveIntegerField(default=0)
