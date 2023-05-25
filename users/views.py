75756975fe4d3aebf139244fe065d122cfd8ca92
Yang ye lin <oky07031217@gmail.com>
2023-05-25 22:01:13 +0900

    Fix

--- users/views.py
+++ users/views.py
@@ -12,21 +12,24 @@ from json.decoder import JSONDecodeError
 from rest_framework import status
 from users.models import User
 from rest_framework.views import APIView
-from users.serializers import UserProfileSerializer, SocialLoginSerializer
+from users.serializers import UserProfileSerializer
 from rest_framework.generics import get_object_or_404
 from rest_framework import permissions
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import HttpResponseRedirect
+
 
 BASE_URL = 'http://127.0.0.1:8000/'
 KAKAO_CALLBACK_URI = BASE_URL + 'users/kakao/callback/'
 GOOGLE_CALLBACK_URI = BASE_URL + 'users/google/callback/'
 
 state = os.environ.get('STATE')
-state = os.environ.get('STATE')
 
-# KAKAO_REST_API_KEY json파일 형태로 보관하여 연결
-
+# token 발급
+def generate_jwt_token(user):
+    refresh = RefreshToken.for_user(user)
+    return {'refresh': str(refresh), 'access': str(refresh.access_token)}
 
 def kakao_login(request):
     rest_api_key = os.environ.get('KAKAO_REST_API_KEY')
@@ -119,75 +122,50 @@ class KakaoLogin(SocialLoginView):
     callback_url = KAKAO_CALLBACK_URI
 
 
-def generate_jwt_token(user):
-    access_token = AccessToken.for_user(user)
-    return str(access_token)
-
-
+# Google 로그인
 def google_login(request):
     scope = "https://www.googleapis.com/auth/userinfo.email"
     client_id = getattr(settings, "SOCIAL_AUTH_GOOGLE_CLIENT_ID")
     return redirect(f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={scope}")
 
-
-
 def google_callback(request):
     client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
     client_secret = os.environ.get("SOCIAL_AUTH_GOOGLE_SECRET")
     code = request.GET.get('code')
-
-
-    print(f'code = {code}')
-
-
+ 
     # 1. 받은 code로 구글에 access token 요청
     token_req = requests.post(
         f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}&state={state}")
 
-
     # 1-1. json으로 변환 & 에러 부분 파싱
     token_req_json = token_req.json()
     error = token_req_json.get("error")
-    print("###################")
-    print(token_req_json)
-    print("###################")
+
     # 1-2. 에러 발생 시 종료
     if error is not None:
         raise JSONDecodeError(error)
 
-
     # 1-3. 성공 시 access_token 가져오기
     access_token = token_req_json.get('access_token')
-
-
-    print(f'access_token  = {access_token }')
-
 
     # 2. 가져온 access_token으로 이메일값을 구글에 요청
     email_req = requests.get(
         f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
     email_req_status = email_req.status_code
 
-
     # 2-1. 에러 발생 시 400 에러 반환
     if email_req_status != 200:
         return JsonResponse({'err_msg': 'failed to get email'}, status=status.HTTP_400_BAD_REQUEST)
 
-
     # 2-2. 성공 시 이메일 가져오기
     email_req_json = email_req.json()
     email = email_req_json.get('email')
-
-
-    print(email)
-
-
+   
     # 3. 전달받은 이메일, access_token, code로 회원가입 or 로그인 진행
     try:
         # 전달받은 이메일로 등록된 유저가 있는지 탐색
         user = User.objects.get(email=email)
         social_user = SocialAccount.objects.get(user=user)
-
 
         # 소셜 유저가 아니거나 소셜 유저이지만 구글계정이 아닐 때 에러처리
         if social_user is None:
@@ -195,44 +173,23 @@ def google_callback(request):
         if social_user.provider != 'google':
             return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
 
-
         # 기존에 Google로 가입된 유저 => 로그인 & 해당 유저의 jwt 발급
         data = {'access_token': access_token, 'code': code}
-        print(data)
-
-
         accept = requests.post(
             f"{BASE_URL}users/google/login/finish/", data=data)
         accept_status = accept.status_code
 
-
         # 뭔가 중간에 문제가 생기면 에러
         if accept_status != 200:
             return JsonResponse({'err_msg': 'failed to signin'}, status=accept_status)
-
-
-        accept_json = accept.json()
-        accept_json.pop('user', None)
-        print("#################################")
-        print(accept_json)
+        
+        # JWT 토큰 발급
         jwt_token = generate_jwt_token(user)
-        accept_json['token'] = jwt_token
-        # token = SocialLoginSerializer.get_token(user)
-        # refresh_token = str(token)
-        # access_token = str(token.access_token)  # JWT 생성
-        # response = Response(
-        #     {
-        #         "message": "로그인 성공!!",
-        #         "token": {
-        #             "access": access_token,
-        #             "refresh": refresh_token,
-        #         },
-        #     },
-        #     status=status.HTTP_200_OK,
-        # )
-        # return response
-        return JsonResponse(accept_json,status=status.HTTP_200_OK ) # 여기를 바꿔서 토큰을 받아오기
-
+        response = HttpResponseRedirect("http://127.0.0.1:5500/index.html")
+        response.set_cookie('jwt_token', jwt_token)
+        return response
+    
+    
     except User.DoesNotExist:
         # 기존에 해당 이메일로 가입된 유저가 없으면 새로 가입 => 새로 회원가입 & 해당 유저의 jwt 발급
         data = {'access_token': access_token, 'code': code}
@@ -240,23 +197,25 @@ def google_callback(request):
             f"{BASE_URL}users/google/login/finish/", data=data)
         accept_status = accept.status_code
 
-
         if accept_status != 200:
             return JsonResponse({'err_msg': 'failed to signup'}, status=accept_status)
 
-        accept_json = accept.json()
-        accept_json.pop('user', None)
-        return JsonResponse(data)
+      # JWT 토큰 발급
+        jwt_token = generate_jwt_token(user)
+        response = HttpResponseRedirect("http://127.0.0.1:5500/index.html")
+        response.set_cookie('jwt_token', jwt_token)
+        return response
 
 
 class GoogleLogin(SocialLoginView):
     adapter_class = google_view.GoogleOAuth2Adapter
     callback_url = GOOGLE_CALLBACK_URI
     client_class = OAuth2Client
-    print(adapter_class)
+
+
 
 class MyPage(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, user_id):
         user_profile = get_object_or_404(User, id=user_id)
