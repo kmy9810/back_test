74bc99f6149155de94848f1cf3ddf062eba60ece
miyeong <kmy9810@naver.com>
2023-06-07 21:01:57 +0900

    배포테스트

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,5 +1,7 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
+
+from users.models import User
 from .models import Recipe, Review, Comment
 from .serializers import RecipeSerializer, ReviewSerializer, CommentSerializer, SearchSerializer
 from faker import Faker
@@ -15,10 +17,11 @@ class RecipeTest(APITestCase):
                                                  tip=cls.faker.word(),ingredients=cls.faker.sentence(),
                                                      tag=cls.faker.word(), car=cls.faker.word(), na=cls.faker.word(),
                                                      fat=cls.faker.word(), pro=cls.faker.word()))
-        # cls.user = User.objects.create_user(username='mimi', email='miyeong@naver.com', password='1234')
+        cls.user = User.objects.create_user(username='mimi', email='miyeong@naver.com', password='1234')
+        cls.data = {'email':"miyeong@naver.com", "password":"1234"}
 
-    # def setUp(self):
-    #     self.access_token = self.client.post(reverse('token_obtain_pair'), self.data).data['access']
+    def setUp(self):
+        self.access_token = self.client.post(reverse('token_obtain_pair'), self.data).data['access']
 
     def test_get_recipe_list(self):
         response = self.client.get(
@@ -37,7 +40,7 @@ class RecipeTest(APITestCase):
     def test_patch_recipe_detail(self):
         for recipe in self.recipes:
             url = recipe.get_absolute_url(category='recipe_detail')
-            response = self.client.patch(path=url,
+            response = self.client.patch(path=url,HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
                                          data={'name': self.faker.sentence()})
             self.assertEquals(response.status_code, 200)
 
@@ -58,11 +61,16 @@ class ReviewTest(APITestCase):
                                                      tip=cls.faker.word(), ingredients=cls.faker.sentence(),
                                                      tag=cls.faker.word(), car=cls.faker.word(), na=cls.faker.word(),
                                                      fat=cls.faker.word(), pro=cls.faker.word()))
+            cls.user = User.objects.create_user(username='mimi', email='miyeong@naver.com', password='1234')
+            cls.data = {'email': "miyeong@naver.com", "password": "1234"}
+
+    def setUp(self):
+        self.access_token = self.client.post(reverse('token_obtain_pair'), self.data).data['access']
 
     def test_post_review(self):
         for recipe in self.recipes:
             url = recipe.get_absolute_url(category='review')
-            response = self.client.post(path=url,
+            response = self.client.post(path=url,HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
                                         data={'title': self.faker.sentence(),
                                               'content': self.faker.sentence(), 'star': "1",
                                               'image': 'undefined'})
